070a9c1391acb627752ea2e85aa2019a4095b1d3
matiash26 <matias.henri26@gmail.com>
2023-04-20 22:39:02 -0300

    footer adicionado

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 import Home from "./Home"
 import "./App.css"
 function App() {
   return (
     <div className="App">
+      <Header />
       <Home />
+      <Footer />
     </div>
   )
 }
