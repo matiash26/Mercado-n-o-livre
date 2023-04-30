dbf96be53f22989feee3261e53f6d48496a41e8a
matiash26 <matias.henri26@gmail.com>
2023-04-30 15:03:35 -0300

    deixando o home mais limpo e arrumando o submenu

---
+++ src/helpers/products.js
@@ -0,0 +1,155 @@
+import dell from "../assets/image/dell.webp"
+import watch from "../assets/image/watch.avif"
+import iphone from "../assets/image/iphone.webp"
+import tablet from "../assets/image/tablet.webp"
+import cam from "../assets/image/cam.webp"
+
+export const product = [
+    {
+        nome: "Smartwatch Samsung Galaxy Watch",
+        preco: 1599,
+        foto: watch,
+        descricao: "Smartwatch Samsung Galaxy Watch Active 2",
+        parcelas: "10x R$ 10 sem juros"
+    },
+    {
+        nome: "Smartphone Apple iPhone 13",
+        preco: 6999,
+        foto: iphone,
+        descricao: "Smartphone Apple iPhone 13, 128GB",
+        parcelas: "10x R$ 700 sem juros"
+    },
+    {
+        nome: "Tablet Samsung Galaxy Tab S7+",
+        preco: 4599,
+        foto: tablet,
+        descricao: "Tablet Samsung Galaxy Tab S7+, 256GB",
+        parcelas: "10x R$ 460 sem juros"
+    },
+    {
+        nome: "Notebook Dell Inspiron 15",
+        foto: dell,
+        preco: 3270,
+        descricao: "Notebook Dell Inspiron 15, Intel Core i5, 8GB RAM",
+        parcelas: "10x R$ 327 sem juros"
+
+    },
+    {
+        nome: "Câmera Canon EOS R6",
+        preco: 1399,
+        foto: cam,
+        descricao: "Câmera Canon EOS R6 com Lente 24-105mm f/4L",
+        parcelas: "12x R$ 1.366,58 sem juros"
+    },
+    {
+        nome: "Notebook Dell Inspiron 15",
+        foto: dell,
+        preco: 3270,
+        descricao: "Notebook Dell Inspiron 15, Intel Core i5, 8GB RAM",
+        parcelas: "10x R$ 327 sem juros"
+
+    },
+    {
+        nome: "Smartwatch Samsung Galaxy Watch",
+        preco: 1599,
+        foto: watch,
+        descricao: "Smartwatch Samsung Galaxy Watch Active 2",
+        parcelas: "10x R$ 10 sem juros"
+    },
+    {
+        nome: "Smartphone Apple iPhone 13",
+        preco: 6999,
+        foto: iphone,
+        descricao: "Smartphone Apple iPhone 13, 128GB",
+        parcelas: "10x R$ 700 sem juros"
+    },
+    {
+        nome: "Tablet Samsung Galaxy Tab S7+",
+        preco: 4599,
+        foto: tablet,
+        descricao: "Tablet Samsung Galaxy Tab S7+, 256GB",
+        parcelas: "10x R$ 460 sem juros"
+    },
+    {
+        nome: "Notebook Dell Inspiron 15",
+        foto: dell,
+        preco: 3270,
+        descricao: "Notebook Dell Inspiron 15, Intel Core i5, 8GB RAM",
+        parcelas: "10x R$ 327 sem juros"
+
+    },
+    {
+        nome: "Smartwatch Samsung Galaxy Watch",
+        preco: 1599,
+        foto: watch,
+        descricao: "Smartwatch Samsung Galaxy Watch Active 2",
+        parcelas: "10x R$ 10 sem juros"
+    },
+    {
+        nome: "Smartphone Apple iPhone 13",
+        preco: 6999,
+        foto: iphone,
+        descricao: "Smartphone Apple iPhone 13, 128GB",
+        parcelas: "10x R$ 700 sem juros"
+    },
+    {
+        nome: "Tablet Samsung Galaxy Tab S7+",
+        preco: 4599,
+        foto: tablet,
+        descricao: "Tablet Samsung Galaxy Tab S7+, 256GB",
+        parcelas: "10x R$ 460 sem juros"
+    },
+    {
+        nome: "Notebook Dell Inspiron 15",
+        foto: dell,
+        preco: 3270,
+        descricao: "Notebook Dell Inspiron 15, Intel Core i5, 8GB RAM",
+        parcelas: "10x R$ 327 sem juros"
+
+    },
+    {
+        nome: "Câmera Canon EOS R6",
+        preco: 1399,
+        foto: cam,
+        descricao: "Câmera Canon EOS R6 com Lente 24-105mm f/4L",
+        parcelas: "12x R$ 1.366,58 sem juros"
+    },
+    {
+        nome: "Notebook Dell Inspiron 15",
+        foto: dell,
+        preco: 3270,
+        descricao: "Notebook Dell Inspiron 15, Intel Core i5, 8GB RAM",
+        parcelas: "10x R$ 327 sem juros"
+
+    },
+    {
+        nome: "Smartwatch Samsung Galaxy Watch",
+        preco: 1599,
+        foto: watch,
+        descricao: "Smartwatch Samsung Galaxy Watch Active 2",
+        parcelas: "10x R$ 10 sem juros"
+    },
+    {
+        nome: "Smartphone Apple iPhone 13",
+        preco: 6999,
+        foto: iphone,
+        descricao: "Smartphone Apple iPhone 13, 128GB",
+        parcelas: "10x R$ 700 sem juros"
+    },
+    {
+        nome: "Tablet Samsung Galaxy Tab S7+",
+        preco: 4599,
+        foto: tablet,
+        descricao: "Tablet Samsung Galaxy Tab S7+, 256GB",
+        parcelas: "10x R$ 460 sem juros"
+    },
+    {
+        nome: "Notebook Dell Inspiron 15",
+        foto: dell,
+        preco: 3270,
+        descricao: "Notebook Dell Inspiron 15, Intel Core i5, 8GB RAM",
+        parcelas: "10x R$ 327 sem juros"
+
+    },
+
+]
